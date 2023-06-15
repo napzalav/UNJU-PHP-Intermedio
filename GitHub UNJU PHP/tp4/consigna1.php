bae1f3f815dc221175bfc93a154e36e457049196
napzalav <cesaralavila@hotmail.com>
2023-06-15 18:43:16 -0300

    carpeta tp4 + consigna1

---
+++ GitHub UNJU PHP/tp4/consigna1.php
@@ -0,0 +1,20 @@
+<?php
+//trim() elimina los espacios al principio y al final
+//strlen() devuelve la longitud de una cadena de texto. Es util para determinar la cantidad de caracteres presentes en una cadena, lo cual puede ser utilizado para realizar validaciones o ajustes de formato.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $cadena = $_POST["cadena"]; //"cadena" es el nombre del campo del formulario del cual queremos obtener la cadena de caracteres. cuando se ingresa se almacena como un array dentro de la variable llamada $cadena
+}
+
+function controlar(){
+
+}
+
+
+
+
+
+
+
+
+
+?>
