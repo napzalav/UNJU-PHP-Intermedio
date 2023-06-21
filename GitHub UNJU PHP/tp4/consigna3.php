846f6365e2cef30cc8edff10c06a215ec4c2466f
napzalav <cesaralavila@hotmail.com>
2023-06-21 19:13:10 -0300

    cambio consigna3.php

--- GitHub UNJU PHP/tp4/consigna3.php
+++ GitHub UNJU PHP/tp4/consigna3.php
@@ -9,7 +9,7 @@ if (isset($_POST) && !empty($_POST)) { //verificamos que se haya enviado un emai
 
     if (strpos($email, "@") === false) { //usando la funcion strpos() buscamos el simbolo "@" dentro de la cadena "$email". Si esta condición arroja "false" significa q el simbolo "@" no está presente en la cadena "$email" y arroja el siguiente mensaje:
         echo "email incorrecto, no se encuentra el símbolo @";
-    } elseif (strpos($email, "@") == 0) { //esta condicion verifica si el simbolo "@" se encuentra en la posicion [0] o sea al principio de la cadena "$email". Si devuelve 0, significa que el simbolo "@" se encuentra al principio por lo que entendemos que el email esta incompleto y falta el nombre, arrojando el sguiente mensaje:
+    } elseif (strpos($email, "@") === 0) { //esta condicion verifica si el simbolo "@" se encuentra en la posicion [0] o sea al principio de la cadena "$email". Si devuelve 0, significa que el simbolo "@" se encuentra al principio por lo que entendemos que el email esta incompleto y falta el nombre, arrojando el sguiente mensaje:
         echo "email incompleto, por favor corrija su email";
     } else { //si el email TIENE el simbolo "@" y este NO SE ENCUENTRA al principio de la cadena entonces significa q es un email válido
         echo "El email es válido";
