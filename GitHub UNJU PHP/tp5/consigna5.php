add2b464fbfae4d6828cddb4e4abc24211aa1b53
napzalav <cesaralavila@hotmail.com>
2023-06-28 17:06:53 -0300

    se completó consigna5.php

--- GitHub UNJU PHP/tp5/consigna5.php
+++ GitHub UNJU PHP/tp5/consigna5.php
@@ -106,18 +106,26 @@ if (isset($_POST["submit"])) {
     echo "<b>Estado del celular1: </b>" . $celular1->getEstado() . "<br>";
     echo "<b>Estado del celular2: </b>" . $celular2->getEstado() . "<br>";
 
-    //8-Simule el siguiente comportamiento con el celular 1: ***Apague el celular***Haga una llamada***Termine la llamada***Encienda el celular***Termine la llamada(apagarCelular)***Imprima el estado del celular1
-    echo "<b>Esta es una simulación de comportamiento con el celular1: </b><br>";
+    //8-Simule el siguiente comportamiento con el celular 1: ***Apague el celular***Haga una llamada***Termine la llamada***Encienda el celular***Termine la llamada***Imprima el estado del celular1***
+    echo "<b>Esta es una simulación de comportamiento con el celular1 (datos ingresados mediante formulario): </b><br>";
+    echo "*Apague el celular*";
     echo $celular1->apagarCelular();
+    echo "*Haga una llamada*";
     echo $celular1->hacerLlamada($numerollamado);
+    echo "*Termine la llamada*";
     echo $celular1->terminarLlamada($numerollamado);
+    echo "*Encienda el celular*";
     echo $celular1->encenderCelular();
+    echo "*Termine la llamada*";
     echo $celular1->terminarLlamada($numerollamado);
+    echo "*Imprima el estado del celular1*";
     echo "<br>Estado del celular1: " . $celular1->getEstado() . "<br>";
 
     //9-Simule el siguiente comportamiento con el celular2: ***Haga una llamada***Imprima el estado del celular2
-    echo "<b>Esta es una simulación de comportamiento con el celular2: </b><br>";
-    $celular2->hacerLlamada($numerollamado);
+    echo "<b>Esta es una simulación de comportamiento con el celular2 (datos hardcodeados): </b><br>";
+    echo "*Haga una llamada*";
+    echo $celular2->hacerLlamada($numerollamado);
+    echo "*Imprima el estado del celular2*";
     echo "<br>Estado del celular2: " . $celular2->getEstado() . "<br>";
 
 }
