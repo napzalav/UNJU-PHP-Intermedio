2149f1e1418912bd526ba8cf5f788e95c7a4afae
napzalav <cesaralavila@hotmail.com>
2023-07-04 22:33:50 -0300

    se agrega formulario, menu + style

---
+++ GitHub UNJU PHP/tp6/formulario.php
@@ -0,0 +1,36 @@
+<form action="desarrollo1.php" method="post" enctype="multipart/form-data">
+        <label for="nombre">Nombre y Apellido</label>
+        <input type="text" name="nombre" required><br>
+        
+        <label for="nacimiento">Fecha de nacimiento</label>
+        <input type="date" name="nacimiento" required><br>
+
+        <label for="direccion">Dirección</label>
+        <input type="text" name="direccion" required><br>
+
+        <label for="sexo">Sexo</label>
+        <select name="sexo" id="sexo" required>
+            <option value="masculino">Masculino</option>
+            <option value="femenino">Femenino</option>
+        </select><br>
+        
+        <label for="disponibilidad">Disponibilidad</label>
+        <select name="disponibilidad" id="disponibilidad" required>
+            <option value="Part time">Part time</option>
+            <option value="Full time">Full time</option>
+        </select><br>
+
+        <label for="puesto">Puesto</label>
+        <select name="puesto" id="puesto" required>
+            <option value="Informática">Informática</option>
+            <option value="Contable">Contable</option>
+            <option value="RRHH">RRHH</option>
+        </select><br>
+
+        <label for="cv">Cargar CV (.pdf, .doc, .jpg)</label>
+        <input type="file" name="cv" id="cv" accept=".pdf, .doc, .jpg" required><br>
+
+        <br>
+        <input type="submit" name="enviar" value="Enviar postulación">
+    </form>
+    <br>
---
+++ GitHub UNJU PHP/tp6/menu.php
@@ -0,0 +1,7 @@
+<p>Click para visualizar las otras consignas:</p>
+<ul>
+    <li><a href="./consigna1.php">Consigna 1</a></li>
+    <li><a href="./consigna2.php">Consigna 2</a></li>
+    <li><a href="./consigna3.php">Consigna 3</a></li>
+    <li><a href="./consigna4.php">Consigna 4</a></li>
+</ul>
