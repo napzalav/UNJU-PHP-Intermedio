c99e24c94bb9b492a87d93fa3d05dfdb9c494f58
napzalav <cesaralavila@hotmail.com>
2023-07-06 23:34:58 -0300

    add consigna3.php

---
+++ GitHub UNJU PHP/tp6/consigna3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabajo Práctico 6</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>“PILARES DE LA PROGRAMACIÓN ORIENTADA A OBJETOS”</h1>
+    
+    <?php include 'menu.php'; ?>
+    
+    <h3>Consigna 3</h3>
+    <p>Agregue un método estático a la clase Empleado del punto anterior que permita mostrar los puestos existentes (informática, contable, rrhh). Muestre por pantalla el resultado de ejecutar dicho método.</p>
+
+    <h3>***********DESARROLLO***********</h3>
+
+    <?php 
+    include 'desarrollo3.php';
+    ?>
+
+</body>
+</html>
