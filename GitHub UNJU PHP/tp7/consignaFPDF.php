cd0e95b68c28b470898ed5b41ab7737e71254dd0
napzalav <cesaralavila@hotmail.com>
2023-08-04 12:13:11 -0300

    update tp7- se identó correctamente el codigo

--- GitHub UNJU PHP/tp7/consignaFPDF.php
+++ GitHub UNJU PHP/tp7/consignaFPDF.php
@@ -1,48 +1,49 @@
 <?php
 require('libraries/fpdf186/fpdf.php');
 
-class PDF extends FPDF{
-// Cabecera de página
-function Header(){
-    // Logo
-    $this->Image('assets/avatar.png',10,8,25);
-    // configuracion para la cabecera
-    $this->SetFont('Arial','B',30);
-    // Cabecera
-    $this->Cell(0,10,'Argentina Programa',0,0,'C');
-    // Salto de línea
-    $this->Ln(40);
+class PDF extends FPDF
+{
+    // Cabecera de página
+    function Header()
+    {
+        // Logo
+        $this->Image('assets/avatar.png', 10, 8, 25);
+        // configuracion para la cabecera
+        $this->SetFont('Arial', 'B', 30);
+        // Cabecera
+        $this->Cell(0, 10, 'Argentina Programa', 0, 0, 'C');
+        // Salto de línea
+        $this->Ln(40);
 
-    //configuracion para el titulo
-    $this->SetFont('Times','I',24);
-    $this->Cell(30,10,'FPDF',0,0,'L');
-    $this->Ln();
+        //configuracion para el titulo
+        $this->SetFont('Times', 'I', 24);
+        $this->Cell(30, 10, 'FPDF', 0, 0, 'L');
+        $this->Ln();
 
-    //configuracion para el cuerpo
-    $this->SetFont('Times','I',14);
-    //$this->SetFont('','I');
+        //configuracion para el cuerpo
+        $this->SetFont('Times', 'I', 14);
+        //$this->SetFont('','I');
 
-// Imprimimos el texto justificado
-$this->MultiCell(0,10,'FPDF es una clase escrita en PHP que permite generar documentos PDF directamente desde PHP, es decir, sin usar la biblioteca PDFlib. La F de FPDF significa Free (gratis y libre): puede usted usarla para cualquier proposito y modificarla a su gusto para satisfacer sus necesidades.',1);
-}
+        // Imprimimos el texto justificado
+        $this->MultiCell(0, 10, 'FPDF es una clase escrita en PHP que permite generar documentos PDF directamente desde PHP, es decir, sin usar la biblioteca PDFlib. La F de FPDF significa Free (gratis y libre): puede usted usarla para cualquier proposito y modificarla a su gusto para satisfacer sus necesidades.', 1);
+    }
 
-function Footer(){
-    // Posición a 1,5 cm del final
-    $this->SetY(-15);
-    // Arial itálica 8
-    $this->SetFont('Arial','I',14);
-    // Color del texto en gris
-    $this->SetTextColor(100);
-    // Número de página
-    $this->Cell(0,10,'Cesar Alavila',1,0,'C');
-}
-
+    function Footer()
+    {
+        // Posición a 1,5 cm del final
+        $this->SetY(-15);
+        // Arial itálica 8
+        $this->SetFont('Arial', 'I', 14);
+        // Color del texto en gris
+        $this->SetTextColor(100);
+        // Número de página
+        $this->Cell(0, 10, 'Cesar Alavila', 1, 0, 'C');
+    }
 }
 
 $pdf = new PDF();
 $pdf->AddPage(); //agregar una nueva pagina
-$pdf->SetFont('Arial','B',16); //se pasa por parametros la FUENTE, TIPO y TAMAÑO
-
+$pdf->SetFont('Arial', 'B', 16); //se pasa por parametros la FUENTE, TIPO y TAMAÑO
 
 $pdf->Output();
 
--- GitHub UNJU PHP/tp7/consignaMailer.php
+++ GitHub UNJU PHP/tp7/consignaMailer.php
@@ -13,32 +13,32 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.office365.com';                     //Configuracion SMTP para HOTMAIL
+    $mail->SMTPDebug = 0; //Enable verbose debug output
+    $mail->isSMTP(); //Send using SMTP
+    $mail->Host = 'smtp.office365.com'; //Configuracion SMTP para HOTMAIL
     //$mail->Host       = 'smtp.gmail.com';                     //Configuracion SMTP para GMAIL
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->SMTPAuth = true; //Enable SMTP authentication
 
     /****************************************** MODIFICAR ESTOS DATOS ***********************************************/
-    $mail->Username   = '';                     //SMTP username
-    $mail->Password   = '';                               //SMTP password
+    $mail->Username = ''; //SMTP username
+    $mail->Password = ''; //SMTP password
     /*****************************************************************************************/
 
-    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPSecure = 'tls'; //Enable implicit TLS encryption
+    $mail->Port = 587; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('tu_email@extension.com', 'Tu_nombre'); //Aqui va el remitente, es quien envía el mail, seguido de un nombre opcional
-    $mail->addAddress('receptor_email@extension.com');     //Aqui va el email a quien queremos enviar, es el receptor
+    $mail->addAddress('receptor_email@extension.com'); //Aqui va el email a quien queremos enviar, es el receptor
 
     //Attachments
-    $mail->addAttachment('assets/Practica M2-C4.pdf');         //Agregar al mail un archivo adjunto
-    $mail->addAttachment('assets/kotl.jpg');         //Agregar al mail un archivo adjunto
+    $mail->addAttachment('assets/Practica M2-C4.pdf'); //Agregar al mail un archivo adjunto
+    $mail->addAttachment('assets/kotl.jpg'); //Agregar al mail un archivo adjunto
 
     //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Argentina Programa 2023';     //Asunto
-    $mail->Body    = 'PHPMailer es una clase desarrollada en PHP que trata de facilitar la funcionalidades asociadas a los envíos y manejos de emails. Se basa en el componente active server ASPMail. PHPMailer permite hacer de manera sencilla trabajos complejos, como adjuntar ficheros o realizar envíos de mails con formato HTML. Es muy recomendable realizar los envíos a través del protocolo SMTP';
+    $mail->isHTML(true); //Set email format to HTML
+    $mail->Subject = 'Argentina Programa 2023'; //Asunto
+    $mail->Body = 'PHPMailer es una clase desarrollada en PHP que trata de facilitar la funcionalidades asociadas a los envíos y manejos de emails. Se basa en el componente active server ASPMail. PHPMailer permite hacer de manera sencilla trabajos complejos, como adjuntar ficheros o realizar envíos de mails con formato HTML. Es muy recomendable realizar los envíos a través del protocolo SMTP';
 
     $mail->send();
     echo 'El mensaje se envió correctamente';
