6b6c71bb9bedf5edf531144ce535187c70c5ad89
napzalav <cesaralavila@hotmail.com>
2023-08-10 22:56:40 -0300

    add DB movimientos + index.php

---
+++ GitHub UNJU PHP/tp11/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FINANZA FAMILIAR</title>
+    <style>
+        .wrapper {
+            width: 90%;
+            margin: 0 auto;
+        }
+
+        table tr td:last-child {
+            width: 120px;
+        }
+    </style>
+    <script>
+        $(document).ready(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2>Movimientos contables</h2>
+                        <a href="./create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i>Agregar nuevo movimiento</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
